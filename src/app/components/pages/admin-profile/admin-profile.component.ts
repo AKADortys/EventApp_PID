4d24a894131846430469212952820724949338f5
AKADortys <thibault.a@outlook.com>
2025-05-26 18:21:55 +0200

    suite et fin admin panel

--- src/app/components/pages/admin-profile/admin-profile.component.ts
+++ src/app/components/pages/admin-profile/admin-profile.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { OrganizerActionsEventComponent } from '../../partials/event/organizer-actions-event/organizer-actions-event.component';
+import { UserOverviewComponent } from '../../partials/user/user-overview/user-overview.component';
+import { EventOrganizerListComponent } from '../../partials/event/event-organizer-list/event-organizer-list.component';
 
 @Component({
   selector: 'app-admin-profile',
-  imports: [],
+  imports: [
+    OrganizerActionsEventComponent,
+    UserOverviewComponent,
+    EventOrganizerListComponent,
+  ],
   templateUrl: './admin-profile.component.html',
-  styleUrl: './admin-profile.component.scss'
+  styleUrl: './admin-profile.component.scss',
 })
-export class AdminProfileComponent {
-
-}
+export class AdminProfileComponent {}
